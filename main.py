0e34f711044f4088a7f2205c21a8a96f2e41cd4f
Suer-Tech <suer2505@gmail.com>
2023-08-22 15:05:04 +0500

    get tickers on every chain

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ def get_price(url):
     data = response.json()
     return data
 
+# Получаем все символы и цены с BINANCE_________________________________________________________________________________
 tickers_bin = []
 data_bin = get_price(url_bin)
 for d in data_bin:
     if d['symbol'][4:] == 'USDT':
         tickers_bin.append(d)
 
+# Получаем все символы и цены с BYBIT___________________________________________________________________________________
 tickers_bit = []
 data_bit = get_price(url_bit)
 for data in data_bit:
@@ -27,6 +29,7 @@ for data in data_bit:
         if d['symbol'][4:] == 'USDT':
             tickers_bit.append(d)
 
+# Получаем все символы и цены с OKX_____________________________________________________________________________________
 flag = "0"  # Production trading:0 , demo trading:1
 marketDataAPI = MarketData.MarketAPI(flag=flag)
 # Retrieve the latest price snapshot, best bid/ask price, and trading volume in the last 24 hours
@@ -40,6 +43,7 @@ for data in data_okx['data']:
     if data['instId'][4:] == 'USDT':
           tickers_okx.append(data)
 
+# Получаем все адреса контрактов с 1inch________________________________________________________________________________
 def get_whitelisted_token_prices():
     tickers_1inch = []
     url = "https://api.1inch.dev/price/v1.1/1"
@@ -58,4 +62,48 @@ def get_whitelisted_token_prices():
 
 tickers_1inch = get_whitelisted_token_prices()
 
+# Получаем все тикеры с адресами контрактов с Coingwcko_________________________________________________________________
+def get_all_tickers():
+    url = f'https://api.coingecko.com/api/v3/coins/list?include_platform=true'
+    response = requests.get(url)
+    data = response.json()
+    return data
+#     for d in data:
+#         if 'ethereum' in d['platforms']:
+#             if d['platforms']['ethereum'] == contract_adress:
+#                 all_tickers.append(d['symbol'])
 
+# Запрос адреса контракта для конкретной сети и запись инфо о символе и адресе в массив_________________________________
+def get_contract_adress(sym, chain, array):
+    for token in all_tickers:
+        if sym == token["symbol"]:
+            if chain in token['platforms']:
+                contract_adress = token['platforms'][chain]
+                crypto = {
+                    'symbol': sym,
+                    'contract_adress': contract_adress
+                }
+                array.append(crypto)
+            else:
+                continue
+
+# Создаём массивы под каждую сеть________________________________________________________________________________________
+ethereum = []
+zksync = []
+arbitrum = []
+binance = []
+polygon = []
+avalanche = []
+
+# Получаем все тикеры с адресами контрактов c Сoingwcko_________________________________________________________________
+all_tickers = get_all_tickers()
+
+# Получаем адреса контрактов по символам из массива_____________________________________________________________________
+for d in tickers_bin:
+    get_contract_adress(d['symbol'][:-4].lower(), 'ethereum', ethereum)
+
+# Запрашиваем цены по символу и адресу контракта с разных бирж__________________________________________________________
+for t in tickers_1inch:
+    for e in ethereum:
+        if t == e['contract_adress']:
+            print(e)
