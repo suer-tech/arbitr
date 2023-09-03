e75f68e5b02e33890b9f9bc0d430a0e562246c90
Suer-Tech <102809506+suer-tech@users.noreply.github.com>
2023-09-03 13:06:28 +0500

    add difference calculate

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import time
 import unittest
 import time
+<<<<<<< HEAD
 import requests
 import okx.MarketData as MarketData
 from python_1inch import OneInchExchange
 
+=======
+from json import JSONDecodeError
+
+import requests
+# import okx.MarketData as MarketData
+from python_1inch import OneInchExchange
+
+api_key = "UNfmcKAyoQSZ3iEr27o4SZbr2xx0j6Su"
+>>>>>>> 3a21ae4 (add difference)
 timestamp = int(time.time() * 1000)
 
 url_bin = "https://api.binance.com/api/v3/ticker/price"
 url_bit = "https://api-testnet.bybit.com/v5/market/tickers?category=spot"
 
+<<<<<<< HEAD
+=======
+base_url = "https://api.1inch.dev/token"
+headers = {"Authorization": "UNfmcKAyoQSZ3iEr27o4SZbr2xx0j6Su", "accept": "application/json"}
+
+
+>>>>>>> 3a21ae4 (add difference)
 def get_price(url):
     response = requests.get(url)
     data = response.json()
     return data
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 3a21ae4 (add difference)
 # Получаем все символы и цены с BINANCE_________________________________________________________________________________
 tickers_bin = []
 data_bin = get_price(url_bin)
@@ -30,6 +51,7 @@ for data in data_bit:
         if d['symbol'][4:] == 'USDT':
             tickers_bit.append(d)
 
+<<<<<<< HEAD
 # Получаем все символы и цены с OKX_____________________________________________________________________________________
 flag = "0"  # Production trading:0 , demo trading:1
 marketDataAPI = MarketData.MarketAPI(flag=flag)
@@ -47,6 +69,26 @@ for data in data_okx['data']:
 # Получаем все адреса контрактов с 1inch________________________________________________________________________________
 def get_whitelisted_token_prices():
     tickers_1inch = []
+=======
+
+# # Получаем все символы и цены с OKX_____________________________________________________________________________________
+# flag = "0"  # Production trading:0 , demo trading:1
+# marketDataAPI = MarketData.MarketAPI(flag=flag)
+# # Retrieve the latest price snapshot, best bid/ask price, and trading volume in the last 24 hours
+# result = marketDataAPI.get_tickers(
+#     instType="SPOT"
+# )
+#
+# tickers_okx = []
+# data_okx = result
+# for data in data_okx['data']:
+#     if data['instId'][4:] == 'USDT':
+#           tickers_okx.append(data)
+
+# Получаем все адреса контрактов с 1inch________________________________________________________________________________
+def get_whitelisted_token_prices():
+    result = []
+>>>>>>> 3a21ae4 (add difference)
     url = "https://api.1inch.dev/price/v1.1/1"
     response = requests.get(url, headers={'Authorization': f'UNfmcKAyoQSZ3iEr27o4SZbr2xx0j6Su'})
     if response.status_code == 200:
@@ -55,17 +97,32 @@ def get_whitelisted_token_prices():
     else:
         print("Failed to fetch token prices.")
     for data in prices:
+<<<<<<< HEAD
         tickers_1inch.append(data)
     return tickers_1inch
 
 tickers_1inch = get_whitelisted_token_prices()
 
+=======
+        result.append(data)
+    return result
+
+
+tickers_1inch = get_whitelisted_token_prices()
+
+
+>>>>>>> 3a21ae4 (add difference)
 # Получаем все тикеры с адресами контрактов с Coingwcko_________________________________________________________________
 def get_all_tickers():
     url = f'https://api.coingecko.com/api/v3/coins/list?include_platform=true'
     response = requests.get(url)
     data = response.json()
     return data
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 3a21ae4 (add difference)
 #     for d in data:
 #         if 'ethereum' in d['platforms']:
 #             if d['platforms']['ethereum'] == contract_adress:
@@ -76,15 +133,23 @@ def get_contract_adress(sym, chain, array):
     for token in all_tickers:
         if sym == token["symbol"]:
             if chain in token['platforms']:
+<<<<<<< HEAD
                 contract_adress = token['platforms'][chain]
                 crypto = {
                     'symbol': sym,
                     'contract_adress': contract_adress
+=======
+                contract_address = token['platforms'][chain]
+                crypto = {
+                    'symbol': sym,
+                    'contract_address': contract_address
+>>>>>>> 3a21ae4 (add difference)
                 }
                 array.append(crypto)
             else:
                 continue
 
+<<<<<<< HEAD
 def get_requested_token_prices(tokens):
     url = "https://api.1inch.dev/price/v1.1/1"
 
@@ -102,6 +167,24 @@ def get_requested_token_prices(tokens):
     #         print(f"{token_address}: {price}")
     # else:
     #     print("Failed to fetch token prices.")
+=======
+
+def get_requested_token_prices(tokens):
+    url = "https://api.1inch.dev/price/v1.1/1"
+    payload = {
+        "tokens": tokens,
+        "currency": "USD"
+    }
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 200:
+        prices = response.json()
+        for token_address, price in prices.items():
+            return price
+
+>>>>>>> 3a21ae4 (add difference)
 
 # Создаём массивы под каждую сеть_______________________________________________________________________________________
 ethereum = []
@@ -111,6 +194,7 @@ binance = []
 polygon = []
 avalanche = []
 
+<<<<<<< HEAD
 ethereum_1inch = []
 
 # Получаем все тикеры с адресами контрактов c Сoingwcko_________________________________________________________________
@@ -130,3 +214,54 @@ for t in tickers_1inch:
             print(e)
 
 
+=======
+# Получаем все тикеры с адресами контрактов c Сoingwcko_________________________________________________________________
+all_tickers = get_all_tickers()
+
+
+def get_price_on_chain(chain, chain_array):
+    # Получаем адреса контрактов по символам из массива_____________________________________________________________________
+    for d in tickers_bin:
+        get_contract_adress(d['symbol'][:-4].lower(), chain, chain_array)
+
+    result = []
+    # # Запрашиваем цены по символу и адресу контракта с разных бирж__________________________________________________________
+    for t in tickers_1inch:
+        for e in chain_array:
+            if t == e['contract_address']:
+                try:
+                    e_1inch = e
+                    e_1inch['price'] = get_requested_token_prices([t])
+                    if e_1inch['price'] is not None:
+                        result.append(e_1inch)
+                except JSONDecodeError:
+                    continue
+    return result
+
+
+ethereum_1inch = get_price_on_chain('ethereum', ethereum)
+binance_1inch = get_price_on_chain('binance-smart-chain', binance)
+
+
+def calculate_percentage_difference(value1, value2):
+    # Проверка на деление на ноль
+    if float(value1) == 0:
+        return None
+
+    # Находим разницу между значениями
+    difference = abs(float(value1) - float(value2))
+
+    # Рассчитываем процентное отношение разницы к первому значению
+    percentage_difference = (difference / abs(float(value1))) * 100
+
+    return percentage_difference
+
+
+for token in ethereum_1inch:
+    for coin in tickers_bin:
+        if token['symbol'] == coin['symbol'][:-4].lower():
+            difference = calculate_percentage_difference(token['price'], coin['price'])
+            if difference > 1:
+                print(coin['symbol'][:-4].lower())
+                print(difference)
+>>>>>>> 3a21ae4 (add difference)
